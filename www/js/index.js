9e4fc1112e955acd473e974ed5ff5cd26da31a8b
juanda <juandacorreo@gmail.com>
2016-09-16 12:16:39 +0200

    Botón con alerta

--- www/js/index.js
+++ www/js/index.js
@@ -1,12 +1,19 @@
 var app = {
     // Application Constructor
     initialize: function() {
-        console.log ("inicializando app");
+        console.log("inicializando app");
         document.addEventListener('deviceready', this.onDeviceReady, false);
         console.log("app inicializada");
-    }, 
+        $("#btnfoto").click(function(event) {
+            alert("Ahora me haría la foto");
+        });
+    },
     onDeviceReady: function() {
         console.log("El dispositivo ya está preparado");
+        $("#btnfoto").click(function(event) {
+            alert("Ahora me haría la foto");
+        });
+
     }
 };
 
